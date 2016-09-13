879f9f05124b58d8eeaf9728de2b3c7088eefc7a
georvalg <georvalg@greeny.cs.tlu.ee>
2016-09-13 13:09:59 +0300

    added loop

---
+++ second.php
@@ -0,0 +1,31 @@
+<?php
+
+	$age = 5;
+
+	if ($age<18) {
+		echo "alaealine";
+		
+	} else {
+		echo "taisealine";
+		
+	}
+
+?>
+
+<br>
+
+<?php
+
+	//vanusele vastavalt 
+	//trykime valja sona 'palju'
+	
+	for ( $i=1 ; $i <= $age ; $i= $i + 1) {
+		
+		echo $i."palju";
+		
+	}
+	
+	echo "onne!";
+	
+
+?>
